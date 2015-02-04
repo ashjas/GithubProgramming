f0eeaa607435fb413a8e879712589692ff1b7c95
Ashish Soni <ashjas@gmail.com>
2015-02-04 13:29:37 +0530

    Used my own implementation of Queue therby removing c++ queue dependency :)

--- Designassignment2.cpp
+++ Designassignment2.cpp
@@ -1,13 +1,62 @@
-#include<queue>
 #include<stdio.h>
 #include<stdlib.h>
-using namespace std;
 int * Level,*Parent,Nnodes,Nedges,src,dst;
 struct node
 {
     int child;
     node * next;
 };
+struct Queue
+{
+    int vertex;
+    Queue * tail;
+    Queue * next;
+};
+int isQempty(Queue * Q)
+{
+    if(Q == NULL)
+        return 1;
+    else 
+        return 0;
+}
+void pushToQ(Queue ** QQ, int vert)
+{
+    Queue *Q = *QQ;
+    Queue *node = (Queue*) malloc(sizeof(Queue));
+    node->vertex=vert;
+    if(Q == NULL)
+    {
+        node->next = NULL;
+        node->tail = node;
+        Q = node;
+    }
+    else
+    {
+        node->next = NULL;
+        Q->tail->next = node;
+        Q->tail = node;
+    }
+    *QQ = Q;
+}
+int popQ(Queue ** QQ)
+{
+    if(*QQ == NULL)
+        return -1;
+    else
+    {
+        Queue *Q = *QQ;
+        Queue * node = Q;
+        int val = Q->vertex;
+        Queue * tail = Q->tail;
+        Q = Q->next;
+        if(Q)
+            Q->tail = tail;
+        free(node);
+        *QQ = Q;
+        return val;
+    }
+
+}
 node** AdjLst;
 int isPathToSource(int start)
 {
@@ -24,17 +73,17 @@ int isPathToSource(int start)
 void BFS(int start)
 {
     int i,j,foundPath=-1;
-    queue<int> Q;
+    Queue *Q = NULL;
     for (i = 0; i < Nnodes; i++)
     {
         Parent[i] = Level[i] = -1;
     }
     Level[start] = Parent[start] = 0;
-    Q.push(start);
-    while(!Q.empty())
+    pushToQ(&Q,start);
+    while(!isQempty(Q))
     {
-        int explore = Q.front();
-        Q.pop();
+        int explore = popQ(&Q);
+        if(explore == -1) break;
         node * t = AdjLst[explore];
         while(t)
         {
@@ -42,7 +91,7 @@ void BFS(int start)
             {
                 Level[t->child] = Level[explore] + 1;
                 Parent[t->child] = explore;  
-                Q.push(t->child);
+                pushToQ(&Q,t->child);
             }
             t=t->next;
         }
