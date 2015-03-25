812d5a6364ee51d7adb6bf1324f5b0d9f69249e1
Ashish Soni <ashjas@gmail.com>
2015-03-25 12:13:39 +0530

    gridPaths done, all conditons were not being checked earlier.

--- dynamicProgramming/gridPaths.c
+++ dynamicProgramming/gridPaths.c
@@ -1,39 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-int main()
+void print(int ar[][6])
 {
     int i,j;
-    int ar[11][6] = {
-                        0,0,0,0,0,0,
-                        0,0,-1,0,0,0,
-                        0,0,0,0,0,0,
-                        0,-1,0,0,0,0,
-                        0,0,-1,0,0,0,
-                        0,0,0,0,0,0,
-                        //0,0,-1,0,-1,0,
-                        0,0,-1,0,0,0,
-                        0,0,0,0,-1,0,
-                        0,0,-1,0,0,0,
-                        0,0,-1,0,0,0
-                    };
-    ar[10][0] = 1;
-    for(i=10;i>=0;i--)
-    {
-        for(j=0;j<6;j++)
-        {
-            if(i == 10 && ar[i][j] != 1/*this skips ar[10][0] so [j-1] is not -1 out of bound*/)/*for bottom row.*/
-                ar[i][j] = ar[i][j-1];
-            if(j == 0  && ar[i][j] != 1/*this skips ar[10][0] so [i+1] is not 11 out of bound.*/)/*for left column and bottom row.*/
-                ar[i][j] = ar[i+1][j];
-            else if(ar[i][j] == 0)
-            {
-                ar[i][j] = ar[i][j-1] + ar[i+1][j];
-            }
-            else if(ar[i][j] == -1)/*these are holes*/
-                ar[i][j] =  0;
-        }
-    }
     for(i=0;i<11;i++)
     {
         for(j=0;j<6;j++)
@@ -42,5 +11,59 @@ int main()
         }
         printf("\n");
     }
+}
+int main()
+{
+    int i,j;
+    int ar[11][6] = {
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        //0,0,-1,0,-1,0,
+                        -1,0,-1,0,-1,0,
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        -1,0,0,0,0,0,
+                        0,0,0,0,0,0
+                    };
+    if(ar[10][0] != -1)
+        ar[10][0] = 1;
+    else 
+        ar[10][0] = 0;
+    print(ar);
+    printf("\n");
+    for(i=10;i>=0;i--)
+    {
+        for(j=0;j<6;j++)
+        {
+            if(i==10 && j>0 )
+            {
+                if(ar[i][j] != -1)
+                    ar[i][j] = ar[i][j-1];
+                else
+                    ar[i][j] = 0;
+            }
+            else if(j==0 && i<10 )
+            {
+                if(ar[i][j] != -1)
+                    ar[i][j] = ar[i+1][j];
+                else
+                    ar[i][j] = 0;
+            }
+            else if(i<10 && j>0 )
+            {
+                if(ar[i][j] != -1)
+                    ar[i][j]= ar[i][j-1] + ar[i+1][j];
+                else
+                    ar[i][j] = 0;
+            }
+            else if(ar[i][j] == -1)
+                ar[i][j] = 0;
+        }
+    }
+    print(ar);
 
 }
