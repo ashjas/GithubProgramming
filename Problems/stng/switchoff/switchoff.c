f95caddb80705b421b74fdb2310f177925a3d696
Ashish Soni <ashjas@gmail.com>
2015-04-17 07:19:54 +0530

    trying left right recursive switchoff.

--- Problems/stng/switchoff/switchoff.c
+++ Problems/stng/switchoff/switchoff.c
@@ -19,6 +19,31 @@ int checkEqMinOptimumPath(int a, int b, int curS, int * ar, int N)
 	}
 	return a;
 }
+int min(int a,int b)
+{
+    return a>b?a:b;
+}
+int mincost(int start,int switches,int N,int *ar)
+{
+    int lw=0, rw=0;
+    if(start-1 >= 0 && switches > 0)
+    {
+        lw = abs(ar[start] - ar[start -1]) * switches;
+        lw += mincost(start -1,switches -1,N,ar);
+    }
+    else lw = 0;
+    if(start+1 < N && switches > 0)
+    {
+        rw = abs(ar[start] - ar[start +1]) * switches;
+        rw += mincost(start +1, switches -1,N,ar);
+    }
+    else rw =0;
+    return min(rw,lw);
+}
+int compare (const void * a, const void * b)
+{
+      return ( *(int*)a - *(int*)b );
+}
 int main()
 {
 	int i, j, k, a, count = 0, T;
@@ -31,13 +56,19 @@ int main()
 		int switchLeft, idxv = 0, I = 0, min = 99999;
 		int totalWatt = 0, steps_moved = 0, freqL = 0, freqR = 0;
 		scanf("%d %d", &N, &S);
-		ar = (int*)malloc(sizeof(int) * N);
+		ar = (int*)malloc((sizeof(int) * (N+1)));
 		switchLeft = N;
 		for (i = 0; i<N; i++)
 		{
 			scanf("%d", &ar[i]);
 		}
-		for (i = 0; i<N; i++)
+                ar[i]=S;
+                qsort(ar,N+1,sizeof(int),compare);
+                for(i=0;i<=N ;i++)
+                    if(ar[i] == S)
+                        break;
+                totalWatt = mincost(i,N,N,ar);
+		/*for (i = 0; i<N; i++)
 		{
 			idxv = 0; I = 0; min = 99999;
 			for (j = 0; j<N; j++)
@@ -62,7 +93,7 @@ int main()
 			switchLeft--;
 			S = idxv;
 			ar[I] = -1;
-		}
+		}*/
 		printf("%d\n", totalWatt);
 	}
 
